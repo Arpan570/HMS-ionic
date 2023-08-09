a26648fdde4d835fa1e0908ec78c28bfd4f73362
Arpan570 <arpansheth570@gmail.com>
2023-08-09 20:38:20 +0530

    does some cleaning

---
+++ src/app/registration-page/insurance-details-patient/insurance-details-patient.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormBuilder, Validators   } from '@angular/forms';
+
+@Component({
+  selector: 'insurance-details-patient',
+  templateUrl: './insurance-details-patient.component.html',
+  styleUrls: ['./insurance-details-patient.component.scss'],
+})
+export class InsuranceDetailsComponent{
+}
--- src/app/registration-page/registration-page.module.ts
+++ src/app/registration-page/registration-page.module.ts
@@ -4,12 +4,15 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 import { RegistrationPageComponent } from './registration-page.component';
+import { PersonalDetailsPageComponent } from './../registration-page/personal-details/personal-details.component';
+import { InsuranceDetailsComponent } from './../registration-page/insurance-details-patient/insurance-details-patient.component';
+import { DemographicsDetailsComponent } from './../registration-page/demographics-details-patient/demographics-details-patient.component';
 import { SharedModule } from './../shared/shared.module';
 
 @NgModule({
   imports: [ CommonModule, FormsModule, IonicModule, ReactiveFormsModule, SharedModule,
     RouterModule.forChild([{ path: '', component: RegistrationPageComponent }])],
-  declarations: [RegistrationPageComponent],
+  declarations: [RegistrationPageComponent, PersonalDetailsPageComponent, InsuranceDetailsComponent, DemographicsDetailsComponent],
   exports: [RegistrationPageComponent]
 })
 export class RegistrationPageModule {}
